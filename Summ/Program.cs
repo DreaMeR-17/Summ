6aa4105f95ff47d6940427a0b907d0742adec461
DreaMeR <DreaMeR@DREAMER>
2024-07-12 13:35:15 +0600

    Добавьте файлы проекта.

---
+++ Summ/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Summ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randomValue = rand.Next(0, 101);
+            int minNumber = 0;
+            int sum = 0;
+
+            for (int i = minNumber; i <= randomValue; i++)
+            {
+                bool dividedByThree = i % 3 == 0;
+                bool dividedByFive = i % 5 == 0;
+
+                if (dividedByThree || dividedByFive)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine($"Получено случайное число {randomValue}, сумма чисел кратных 3 и 5 этого числа равна {sum}.");
+        }
+    }
+}
